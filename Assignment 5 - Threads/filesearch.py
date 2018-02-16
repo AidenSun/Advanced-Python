3e9421616aac505778eb1f1900cc6023153c6968
AidenSun <aidensun113@gmail.com>
2018-02-15 18:07:37 -0800

    Update filesearch.py

--- Assignment 5 - Threads/filesearch.py
+++ Assignment 5 - Threads/filesearch.py
@@ -17,19 +17,19 @@ class FileSearch():
             for f in fileL:
                 self.files.append(tuple((os.path.join(dirPath, f), f)))
         
-        print(self.files)
+        #print(self.files)
 
     def searchName(self, regex, searchString, listResults):
-        #print(regex)
         
         for fileTuple in self.files:
-            if re.search(regex, fileTuple[1]) and strIsInFile(searchString, fileTuple[0]) or len(searchString) == 0:
-                listResults.append(fileTuple)
-                
-        listResults = [fileTuple for fileTuple in self.files if re.search(regex, fileTuple[1]) and strIsInFile(searchString, fileTuple[0]) or len(searchString) == 0]
+            if re.search(regex, fileTuple[1]):
+                if strIsInFile(searchString, fileTuple[0]) or len(searchString) == 0:
+                    listResults.append(fileTuple)
+        
+        #listResults = [fileTuple for fileTuple in self.files if re.search(regex, fileTuple[1]) if strIsInFile(searchString, fileTuple[0]) or len(searchString) == 0]
         listResults.sort(key=lambda tup: tup[0])
         
-        print(listResults)
+        #print(listResults)
         
         #fileNames = (fn for fn in self.files if re.search(regex, fn))
         #return sorted(i for fn in fileNames for i in self.files[fn])
